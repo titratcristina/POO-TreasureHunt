6a5be845f8c7ad9b778c8fef472360c82ff808e8
Titrat Cristina <titratcristina@gmail.com>
2020-05-01 21:44:22 +0300

    Init Seekers

---
+++ Seekers.h
@@ -0,0 +1,14 @@
+//
+// Created by Cristina Titrat on 01/05/2020.
+//
+
+#ifndef POO_TREASUREHUNT_SEEKERS_H
+#define POO_TREASUREHUNT_SEEKERS_H
+
+
+class Seekers {
+
+};
+
+
+#endif //POO_TREASUREHUNT_SEEKERS_H
