8b1d033811b492c10afa1c3a3d96eedfcc7b1341
Titrat Cristina <titratcristina@gmail.com>
2020-05-01 21:40:37 +0300

    Add storyline

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	cout << "Welcome, stranger!\n";
+	cout << "This is the Magic Forest.\n";
+	cout << "You need to define the territory down below.\n";
+
+	int x, y;
+
+	do {
+		cout << "Remember! The map can not be smallar than 15x15.\n";
+		cout << "Choose carefully your numbers: \n";
+		cin >> x >> y;
+	} while (x < 15 || y < 15);
 }
