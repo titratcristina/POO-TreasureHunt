fc8fabc599d80625eb4a9ea3895d120ebfc2bdbc
Titrat Cristina <titratcristina@gmail.com>
2020-05-01 23:47:34 +0300

    Init new class for game management

---
+++ Game.h
@@ -0,0 +1,14 @@
+//
+// Created by Cristina Titrat on 01/05/2020.
+//
+
+#ifndef POO_TREASUREHUNT_GAME_H
+#define POO_TREASUREHUNT_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //POO_TREASUREHUNT_GAME_H
