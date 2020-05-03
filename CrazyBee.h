9e0df6e7d9d22ca7331f2a5fc8660a1f26ef264a
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:25:02 +0300

    Add methods for first player class

--- CrazyBee.h
+++ CrazyBee.h
@@ -6,8 +6,15 @@
 #define POO_TREASUREHUNT_CRAZYBEE_H
 
 
-class CrazyBee {
+#include "Seekers.h"
 
+class CrazyBee : public Seekers {
+private:
+	int direction;  					// used for movement direction
+public:
+	explicit CrazyBee(const Game &G);	// explicit constructor for first player
+	virtual ~CrazyBee();				// virtual destructor
+	void move(const Game &G) override;  // override movement method for first player
 };
 
 
