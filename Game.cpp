c8e480776bb73b4990c6b6e14013576c10c03c97
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 19:41:08 +0300

    Dynamic allocation for map

--- Game.cpp
+++ Game.cpp
@@ -6,3 +6,22 @@
 #include <cstdlib>		// for rand() function
 #include <ctime>		// for time function
 #include "Game.h"
+
+using namespace std;
+
+// in this method the map is initialized
+// the seekers are displayed on corners
+// and the treasures are displayed on map randomly
+Game::Game(int input) {
+	size = input;
+	map = new int *[size];					// dynamic allocation for map
+	for (int i = 0; i < size; i++) {
+		map[i] = new int[size];
+	}
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			map[i][j] = 0;                	// the map is initialized with 0
+		}
+	}
+
+}
