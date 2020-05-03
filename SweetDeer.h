169f8b1418478869eb9cc259439dec82476c42aa
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:55:34 +0300

    Add methods for forth player class

--- SweetDeer.h
+++ SweetDeer.h
@@ -5,10 +5,13 @@
 #ifndef POO_TREASUREHUNT_SWEETDEER_H
 #define POO_TREASUREHUNT_SWEETDEER_H
 
+#include "Seekers.h"
 
-class SweetDeer {
-
+class SweetDeer : public Seekers {
+public:
+	explicit SweetDeer(const Game &G);	// constructor for fourth player
+	virtual ~SweetDeer();				// virtual destructor
+	void move(const Game &G) override;  // override movement method for fourth player
 };
-
 
 #endif //POO_TREASUREHUNT_SWEETDEER_H
