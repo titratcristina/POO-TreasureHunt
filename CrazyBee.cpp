813897f6831691d67ab9ed9241393e12ff3c6e92
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:25:41 +0300

    Add constructor for first player

--- CrazyBee.cpp
+++ CrazyBee.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "CrazyBee.h"
+// constructor for seeker Crazy Bee
+CrazyBee::CrazyBee(const Game &G) : Seekers(G) {
+	position.row = position.column = 0;    	  // initial position is (0, 0)
+	direction = 1;                            // direction is used for movement on the map
+	stuck = false;                            // at the beginning the player isn't stuck
+	index = 1;                                // he has index 1 because he is first
+}
