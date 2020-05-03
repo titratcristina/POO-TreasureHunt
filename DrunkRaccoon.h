66c1e2847893561b8d88ff0ba01ab7c4455280e4
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:44:12 +0300

    Init class for second player

---
+++ DrunkRaccoon.h
@@ -0,0 +1,14 @@
+//
+// Created by Cristina Titrat on 03/05/2020.
+//
+
+#ifndef POO_TREASUREHUNT_DRUNKRACCOON_H
+#define POO_TREASUREHUNT_DRUNKRACCOON_H
+
+
+class DrunkRaccoon {
+
+};
+
+
+#endif //POO_TREASUREHUNT_DRUNKRACCOON_H
