580d0ca332060e12700b90006fbb8e356da4e862
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:23:39 +0300

    Init class for first player

---
+++ CrazyBee.h
@@ -0,0 +1,14 @@
+//
+// Created by Cristina Titrat on 03/05/2020.
+//
+
+#ifndef POO_TREASUREHUNT_CRAZYBEE_H
+#define POO_TREASUREHUNT_CRAZYBEE_H
+
+
+class CrazyBee {
+
+};
+
+
+#endif //POO_TREASUREHUNT_CRAZYBEE_H
