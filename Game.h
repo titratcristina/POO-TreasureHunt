5c5bc1bd3dad33059720109e7e84f1d45a37cf8a
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 19:31:53 +0300

    Add private variables in Game class

--- Game.h
+++ Game.h
@@ -5,10 +5,13 @@
 #ifndef POO_TREASUREHUNT_GAME_H
 #define POO_TREASUREHUNT_GAME_H
 
-
 class Game {
-
+private:
+	int **map;        			// allocate dynamically a 2D matrix
+	int size;        			// map size
+	int diamonds;    			// represents the number of diamonds/treasures found
+	int *inGame;    			// mark the players who are in the game
+	int winner;    				// represents the index of the winning Seeker
 };
-
 
 #endif //POO_TREASUREHUNT_GAME_H
