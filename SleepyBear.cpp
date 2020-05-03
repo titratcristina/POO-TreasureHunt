a800f1e7e3617504b4e2b8c5a2797644a1d21647
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:35:23 +0300

    Add movement method for second player

--- SleepyBear.cpp
+++ SleepyBear.cpp
@@ -11,3 +11,19 @@ SleepyBear::SleepyBear(const Game &G) : Seekers(G) {
 	stuck = false;                    // at the beginning the player isn't stuck
 	index = 2;                        // he has index 2 because he is second
 }
+
+// movement method for second player
+void SleepyBear::move(const Game &G) {
+	// SleepyBear is so lazy that he is skipping a line
+
+	if (position.column + 2 >= 0 && position.column + 2 < G.size &&
+		(G.map[position.row][position.column + 2] == 0 ||
+		 G.map[position.row][position.column + 2] == 5)) {
+
+		// left
+		position.column += 2;
+
+	} else {
+		stuck = true;  // if there is no other way to move the player is stuck
+	}
+}
