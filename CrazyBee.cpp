443101af375c27e3663eeb735913e2236f96b875
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:26:31 +0300

    Add movement method for first player

--- CrazyBee.cpp
+++ CrazyBee.cpp
@@ -10,3 +10,25 @@ CrazyBee::CrazyBee(const Game &G) : Seekers(G) {
 	stuck = false;                            // at the beginning the player isn't stuck
 	index = 1;                                // he has index 1 because he is first
 }
+
+// movement method for first player
+void CrazyBee::move(const Game &G) {
+	// CrazyBee is going down then to the right, forming an L
+
+	if (direction == 1 && position.row + 1 >= 0 && position.row + 1 < G.size &&
+		(G.map[position.row + 1][position.column] == 0 || G.map[position.row + 1][position.column] == 5)) {
+
+		// down
+		position.row += 1;
+
+	} else if (position.column + 1 >= 0 && position.column + 1 < G.size &&
+			   (G.map[position.row][position.column + 1] == 0 || G.map[position.row][position.column + 1] == 5)) {
+
+		// right
+		direction = -1;
+		position.column += 1;
+
+	} else {
+		stuck = true;  // if there is no other way to move the player is stuck
+	}
+}
