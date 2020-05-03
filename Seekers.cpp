3081746d870e460082b4496b9c59b922a443fd26
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 19:56:59 +0300

    Add constructor for seekers

--- Seekers.cpp
+++ Seekers.cpp
@@ -2,4 +2,11 @@
 // Created by Cristina Titrat on 01/05/2020.
 //
 
+#include <iostream>
 #include "Seekers.h"
+
+using namespace std;
+
+// default constructor for seekers
+Seekers::Seekers(const Game &G) {
+}
