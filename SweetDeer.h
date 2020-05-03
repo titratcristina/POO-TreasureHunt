261a3b2f34342cdda82a35ab4921f88249162074
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:48:19 +0300

    Init class for fourth player

---
+++ SweetDeer.h
@@ -0,0 +1,14 @@
+//
+// Created by Cristina Titrat on 03/05/2020.
+//
+
+#ifndef POO_TREASUREHUNT_SWEETDEER_H
+#define POO_TREASUREHUNT_SWEETDEER_H
+
+
+class SweetDeer {
+
+};
+
+
+#endif //POO_TREASUREHUNT_SWEETDEER_H
