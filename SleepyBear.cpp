90b385622404cea8be6038436da6bd30c4e5227b
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:35:00 +0300

    Add constructor for second player

--- SleepyBear.cpp
+++ SleepyBear.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "SleepyBear.h"
+
+// constructor for seeker Sleepy Bear
+SleepyBear::SleepyBear(const Game &G) : Seekers(G) {
+	position.row = G.size - 1;            // initial position is (size - 1, 0)
+	position.column = 0;
+	stuck = false;                    // at the beginning the player isn't stuck
+	index = 2;                        // he has index 2 because he is second
+}
