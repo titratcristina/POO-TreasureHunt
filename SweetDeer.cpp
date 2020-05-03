74d9c07445fc96bb7ad59af016f8f8b4b50c481a
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:57:37 +0300

    Add constructor for forth player

--- SweetDeer.cpp
+++ SweetDeer.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "SweetDeer.h"
+
+// constructor for seeker Sweet Deer
+SweetDeer::SweetDeer(const Game &G) : Seekers(G) {
+	position.row = 0;                // initial position is (0, size - 1)
+	position.column = G.size - 1;
+	stuck = false;                    // at the beginning the player isn't stuck
+	index = 4;                        // he has index 4 because he is fourth
+}
