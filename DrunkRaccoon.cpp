f5064bad36de83c34d19a6fee326a3779c0526a9
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:45:49 +0300

    Add constructor for third player

--- DrunkRaccoon.cpp
+++ DrunkRaccoon.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "DrunkRaccoon.h"
+// constructor for seeker Drunk Raccoon
+DrunkRaccoon::DrunkRaccoon(const Game &G) : Seekers(G) {
+	position.row = G.size - 1;    // initial position is (size-1, size-1)
+	position.column = G.size - 1;
+	direction = 1;                            // direction is used for movement on the map
+	stuck = false;                    // at the beginning the player isn't stuck
+	index = 3;                        // he has index 3 because he is third
+}
