60fe0cb8d4c3c053e71d0aed87e8329df846f28b
Titrat Cristina <titratcristina@gmail.com>
2020-05-03 20:32:55 +0300

    Init class for second player

---
+++ SleepyBear.h
@@ -0,0 +1,14 @@
+//
+// Created by Cristina Titrat on 03/05/2020.
+//
+
+#ifndef POO_TREASUREHUNT_SLEEPYBEAR_H
+#define POO_TREASUREHUNT_SLEEPYBEAR_H
+
+
+class SleepyBear {
+
+};
+
+
+#endif //POO_TREASUREHUNT_SLEEPYBEAR_H
